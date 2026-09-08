1606e6c5bf8a51fcf62ca0033abb95b1a011bb6a
Yaathvik2020 <chinnayaathvik@gmail.com>
2026-09-09 01:54:03 +0530

    Update subnet_id to use first element of public subnet

--- terraform/09-bastion.tf
+++ terraform/09-bastion.tf
@@ -8,7 +8,7 @@
 resource "aws_instance" "bastion" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.bastion_instance_type
-  subnet_id                   = aws_subnet.public.id
+  subnet_id                   = aws_subnet.public[0].id
   vpc_security_group_ids      = [aws_security_group.bastion.id]
   key_name                    = aws_key_pair.bastion.key_name
   associate_public_ip_address = true
