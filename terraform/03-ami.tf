94a019f12f8bf78a9b89c99cc639e02f7049b02f
Yaathvik2020 <chinnayaathvik@gmail.com>
2026-09-09 01:49:18 +0530

    Enhance AMI data source with filters and AZs
    Added filter for specific Ubuntu AMI and availability zones.

--- terraform/03-ami.tf
+++ terraform/03-ami.tf
@@ -8,6 +8,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
 
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
@@ -17,4 +18,12 @@ data "aws_ami" "ubuntu" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  azs = length(var.azs) > 0 ? var.azs : slice(data.aws_availability_zones.available.names, 0, 2)
 }
